15a4c47b553b7ca3d9aa952e9d2100e930f45db3
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2014-03-18 16:02:29 +0100

    popups in progress

--- app/popups.js
+++ app/popups.js
@@ -13,14 +13,40 @@ app.config(function(AnalyticsProvider) {
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/welcomescreen");
+	$urlRouterProvider.otherwise("/welcome-screen");
 	$stateProvider
 		.state('welcome-screen', {
 			url: "/welcome-screen",
 			templateUrl: "popups/welcome-screen.html",
-			pageName: "Weblome Screen",
+			pageName: "Welcome Screen",
 			controller: "WelcomeController"
 		})
+		.state('purchase-dialog', {
+			url: "/purchase-dialog",
+			templateUrl: "popups/purchase-dialog.html",
+			pageName: "Purchase COLT",
+			controller: "PurchaseController"
+		})
+		.state('update-dialog', {
+			url: "/update-dialog",
+			templateUrl: "popups/update-dialog.html",
+			pageName: "Update COLT",
+			controller: "UpdateController"
+		})
+		.state('close-save-dialog', {
+			url: "/close-save-dialog",
+			templateUrl: "popups/close-save-dialog.html",
+			pageName: "Close COLT",
+			controller: "CloseSaveController"
+		})
+});
+
+app.run(function($rootScope, Analytics) {
+	$rootScope.$on('$stateChangeSuccess', function(event, toState){ 
+		$rootScope.pageName = toState.pageName;
+		$rootScope.pageIndex = toState.pageIndex;
+		Analytics.trackPage(toState.url + ".html");
+	});
 });
 
 app.controller("WelcomeController", function($scope, $rootScope, $window) {
@@ -52,3 +78,16 @@ app.controller("WelcomeController", function($scope, $rootScope, $window) {
 		console.log("open project");
 	}
 });
+
+app.controller("PurchaseController", function($scope, $rootScope, $window) {
+	console.log("purchase colt dialog");	
+});
+
+app.controller("UpdateController", function($scope, $rootScope, $window) {
+	console.log("update colt dialog");	
+});
+
+app.controller("CloseSaveController", function($scope, $rootScope, $window) {
+	console.log("close/save colt dialog");	
+});
+
